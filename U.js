3a109ea5ce35958d5ebbbe854f5290dd0fd36d3e
QGB <qgbcs1@gmail.com>
2017-01-09 11:46:38 +0800

    repo

---
+++ U.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+var U
+window.U=U
+
+U.k=99
+function get(url,callback) {
+	
+	xhr.onload = function(){return alert(3)};
+	xhr.open("get", url, true);
+	xhr.send()
+
+	
+	// console.log(this)
+	// su=this.responseText
+	// eval(su)
+}
+
+
+
+
+var hasStrictMode = (function(){ 
+  "use strict";
+  return this == undefined;
+}())
