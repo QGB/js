98f775ed1d4f5584d7c8ab7042185263c0a434d1
QGB <qgbcs1@gmail.com>
2018-08-27 12:02:46 +0800

    2018/08/27周一_12:02

--- site/community.riskiq.com.js
+++ site/community.riskiq.com.js
@@ -1,13 +1,13 @@
 subdomains=[]
-while(btn=document.querySelector('.search-data-dataList-nextPage')){
+btn=document.querySelector('.search-data-dataList-nextPage')
+do{
 	rs=document.querySelectorAll('.search-subdomains-column  > span > span.inlineAction-content > a')
 	for(i of  rs){
 		subdomains.push(i.text)
 	}
 	console.log(subdomains.length)
-
 	btn.click()
-}
+}while(btn=document.querySelector('.search-data-dataList-nextPage'))
 
 
 
