ca1fb10cfe1d53961de3f8aa1dd702ff68002d2c
QGB <qgbcs1@gmail.com>
2018-09-23 13:46:58 +0800

    2018-09-23_13:46

--- site/chinese.samjinnut.co.kr.js
+++ site/chinese.samjinnut.co.kr.js
@@ -1,4 +1,4 @@
-is= document.querySelectorAll('p[align="center"] > input')
+is= document.querySelectorAll(' input[type="image"]')
 
 for(i of is){
 	i.outerHTML='<img src={}></img>'.replace('{}',i.getAttribute('data-src'))
