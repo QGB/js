5944383b72ba074a6433833a8743abf153484dc9
QGB <qgbcs1@gmail.com>
2018-11-28 17:58:10 +0800

    2018/11/28周三_17:58

--- site/www.facebook.com.js
+++ site/www.facebook.com.js
@@ -1,3 +1,14 @@
-alert(233)
-// ds=document.querySelectorAll('div._4-u2')
-// N.postJSON('https://qgb.facebook.com/',subdomains)
+ds=document.querySelectorAll('div._4-u2')
+ts=[]
+for(i of ds){
+	try{
+		t=i.querySelector('._5ptz')
+		ts.push([i.textContent,t])
+	}catch(e){
+		console.log('Error :',i,e)
+	}
+	
+}
+N.postJSON('https://qgb.facebook.com/',ts)
+
+document.title=new Date()
