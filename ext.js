0573d34b3eafaa833842d8d25b36827b37df1c99
QGB <qgbcs1@gmail.com>
2019-05-14 19:25:57 +0800

    qgb-ext

---
+++ ext.js
@@ -0,0 +1,45 @@
+(function(){
+
+
+E.getAllTabs=async function (){
+	var ts=await new Promise(function (resolve, reject) {
+		chrome.tabs.query({},function(tabs){
+			resolve(tabs)
+		})
+    });
+	
+	return ts
+}
+
+
+
+E.test=async function (){
+
+// 	alert(0)
+	console.log(new Date())
+// 	sleep(999)
+	console.log(qgbext,new Date())
+}
+
+return function(){
+	if(window){
+		window.E=E
+		var ext='loaded ext.js '+new Date()
+		console.log(ext)
+		window.qgbext=ext
+	}
+}
+
+
+
+ }())  ()//end of closure
+ 
+if(window){
+	window.E.eval=window.E.weval=function(a){
+		try{
+			return window.eval(a)
+		}catch(e){
+			return e
+		}
+	}
+}
